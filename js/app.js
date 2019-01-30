b61ee199686a61eef7f4d96b805a65e985a14ff5
Maxim Gostev <razial74@gmail.com>
2019-01-30 15:07:52 +0500

    Update 2019-01-30T10:07:46.901Z

--- js/app.js
+++ js/app.js
@@ -172,3 +172,26 @@ $(document).ready(function () {
     }
   });
 }); // Slider End
+
+/*Tabs Begin*/
+
+$('.tabs__tab').on('click', function () {
+  var tabParent = $(this).parent('ul').attr('data-tabs'),
+      tabNumber = $(this).index(),
+      tabTarget = $(document).find('[data-tab="' + tabParent + '"]');
+  $(this).addClass('tabs__tab_active').siblings().removeClass('tabs__tab_active');
+  tabTarget.children().removeClass('tabs__content_active');
+  tabTarget.children().eq(tabNumber).addClass('tabs__content_active');
+});
+$('.tabs-filters__tab').on('click', function () {
+  var tabParent = $(this).parent('ul').attr('data-tabs'),
+      tabFilter = $(this).attr('data-tab-filter'),
+      tabTarget = $(document).find('[data-tab="' + tabParent + '"]');
+  $(this).addClass('tabs-filters__tab_active').siblings().removeClass('tabs-filters__tab_active'); //tabTarget.children().addClass('tabs-filters__content_hidden');
+
+  tabTarget.children().not(".".concat(tabFilter)).fadeOut(300);
+  setTimeout(function () {
+    tabTarget.children(".".concat(tabFilter)).fadeIn(300);
+  }, 300);
+});
+/*Tabs End*/
