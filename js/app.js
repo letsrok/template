21b5f9e25a387bba095ef41e56deae153cf44431
Maxim Gostev <razial74@gmail.com>
2019-02-04 16:14:15 +0500

    Update 2019-02-04T11:14:10.125Z

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
 "use strict";
 
+/* Accordions Begin */
+var DURATION = 250;
+$(document).on('click', '.accordion__header', function (e) {
+  e.preventDefault();
+  var button = $(this),
+      block = button.parents('.accordion'),
+      body = block.find('.accordion__body'),
+      isMultiple = block.parents('.accordions').data('accordion-multiple');
+  body.slideToggle(DURATION);
+  setTimeout(function () {
+    block.toggleClass('is-active');
+  }, DURATION);
+
+  if (!isMultiple) {
+    var siblings = block.siblings('.accordion');
+    siblings.find('.accordion__body').slideUp(DURATION);
+    setTimeout(function () {
+      siblings.removeClass('is-active');
+    }, DURATION);
+  }
+});
+/* Accordions End */
 // Feedback Begin
+
 $('.feedback__input_requied').on('change', function () {
   verifyForm($(this));
 });
